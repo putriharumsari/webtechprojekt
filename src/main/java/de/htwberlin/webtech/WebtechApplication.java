5ec86831473e0f7f7cf7730f9f79477c88e5d984
Putri Harum Sari <92102385+putriharumsari@users.noreply.github.com>
2023-01-08 00:32:00 +0100

    adding okta auth

--- src/main/java/de/htwberlin/webtech/WebtechApplication.java
+++ src/main/java/de/htwberlin/webtech/WebtechApplication.java
@@ -1,15 +1,25 @@
 package de.htwberlin.webtech;
 
+import de.htwberlin.webtech.fileupload.service.FileStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.annotation.Resource;
 
 
 @SpringBootApplication
-public class WebtechApplication {
+public class WebtechApplication implements CommandLineRunner {
+	@Resource
+	FileStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebtechApplication.class, args);
 	}
 
+	@Override
+	public void run(String... arg) throws Exception {
+//    storageService.deleteAll();
+		storageService.init();
+	}
 }
